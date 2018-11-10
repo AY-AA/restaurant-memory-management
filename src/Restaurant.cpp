53da9f4e35ccde98685194b3b80b7325f9fcb1bd
Alex Abramov <alexabramov2610@gmail.com>
2018-11-10 16:21:09 +0200

    Merge remote-tracking branch 'remotes/origin/Alex2'
    # Conflicts:
    #	Customer.cpp
    #	Customer.h
    #	Main.cpp

--- src/Restaurant.cpp
+++ src/Restaurant.cpp
@@ -5,7 +5,6 @@
 #include "../include/Restaurant.h"
 #include "../include/Table.h"
 
-
 Restaurant::Restaurant() = default;
 
 Restaurant::Restaurant(const std::string &configFilePath)
@@ -27,10 +26,8 @@ void Restaurant::start()
         std::vector<std::string> tableInstructions = parseInput(userChoice);
 
         if (firstWord == "open") {
-            //std::vector<std::string> customersToAdd;
+            std::vector<Customer *> customersToAdd;
             int tableId = std::stoi(tableInstructions.at(1));
-            Table *tableToOpen = tables.at(tableId);
-            tableToOpen->openTable();
             std::string name;
             std::string type;
             for (int i = 2; i < tableInstructions.size() - 1; i++) {
@@ -38,22 +35,26 @@ void Restaurant::start()
                 type = tableInstructions.at(i + 1);
                 if (type == "veg") {
                     Customer *toAdd = new VegetarianCustomer(name, nextId);
-                    tableToOpen->addCustomer(toAdd);
+                    customersToAdd.push_back(toAdd);
                     nextId++;
                 } else if (type == "spc") {
                     Customer *toAdd = new SpicyCustomer(name, nextId);
-                    tableToOpen->addCustomer(toAdd);
+                    customersToAdd.push_back(toAdd);
                     nextId++;
                 } else if (type == "chp") {
                     Customer *toAdd = new CheapCustomer(name, nextId);
-                    tableToOpen->addCustomer(toAdd);
+                    customersToAdd.push_back(toAdd);
                     nextId++;
                 } else { //alc
                     Customer *toAdd = new AlchoholicCustomer(name, nextId);
-                    tableToOpen->addCustomer(toAdd);
+                    customersToAdd.push_back(toAdd);
                     nextId++;
                 }
             }
+            OpenTable* op = new OpenTable(tableId, customersToAdd);
+            op->act(*this);
+            actionsLog.push_back(op);
+            delete(op);
         }
         else if(firstWord == "order"){
             std::string::size_type sz;
@@ -61,30 +62,62 @@ void Restaurant::start()
             Order* order = new Order(orderTable);
             actionsLog.push_back(order);
         }
+        else if(firstWord == "order"){
+            std::string::size_type sz;
+            int orderTable = std::stoi (tableInstructions.at(1),&sz);
+            Order* order = new Order(orderTable);
+            order->act(*this);
+            actionsLog.push_back(order);
+        }
         else if(firstWord == "move"){
+            std::string::size_type sz;
+            int srcTable = std::stoi (tableInstructions.at(1),&sz);
+            int destTable = std::stoi (tableInstructions.at(2),&sz);
+            int customerId = std::stoi (tableInstructions.at(3),&sz);
+
+            MoveCustomer* move = new MoveCustomer(srcTable,destTable,customerId);
+            move->act(*this);
+            actionsLog.push_back(move);
 
         }
         else if(firstWord == "close"){
-
+            std::string::size_type sz;
+            int table = std::stoi (tableInstructions.at(1),&sz);
+            Close* close = new Close(table);
+            close->act(*this);
+            actionsLog.push_back(close);
+        }
+        else if(firstWord == "closeall"){
+            CloseAll *closeall = new CloseAll();
+            closeall->act(*this);
+            actionsLog.push_back(closeall);
         }
         else if(firstWord == "menu"){
-
+            PrintMenu *menu = new PrintMenu();
+            menu->act(*this);
+            actionsLog.push_back(menu);
         }
         else if(firstWord == "status"){
-
+            std::string::size_type sz;
+            int table = std::stoi (tableInstructions.at(1),&sz);
+            PrintTableStatus* status = new PrintTableStatus(table);
+            status->act(*this);
+            actionsLog.push_back(status);
         }
         else if(firstWord == "log"){
             PrintActionsLog* log = new PrintActionsLog();
+            log->act(*this);
             actionsLog.push_back(log);
         }
         else if(firstWord == "backup"){
 
         }
         else if(firstWord == "restore"){
-
+//            RestoreResturant * restore = new RestoreResturant();
+//            restore->act();
         }
-        else {//closeall
-
+        else { // bad input
+            std::cout << "bad input" << std::endl;
         }
         std::cin >> userChoice;
         firstWord = userChoice.substr(0, userChoice.find(" "));
@@ -92,37 +125,36 @@ void Restaurant::start()
     while(userChoice != "closeall");
 };
 
-    const std::vector<std::string>& Restaurant::parseInput(std::string &str) {
-        std::vector<std::string> v;
-        int i = 0;
-        while(!str.empty()) {
-            if(str.at(i) == ' '){
-                std::string sub = str.substr(0,str.find(' '));
-                v.push_back(sub);
-                str.erase(0,str.find(' ') + 1);
-                i = 0;
-            }
-            i++;
+const std::vector<std::string>& Restaurant::parseInput(std::string &str) {
+    std::vector<std::string> v;
+    int i = 0;
+    while(i < str.length()) {
+        if(str.at(i) == ' '){
+            std::string sub = str.substr(0, i);
+            v.push_back(sub);
+            str.erase(0,i + 1);
+            i = 0;
         }
-        v.push_back(str);
-        return v;
-    };
+        i++;
+    }
+    v.push_back(str);
+    return v;
+};
 
 int Restaurant::getNumOfTables() const
 {
-
+    return tables.size();
 };
 
 Table* Restaurant::getTable(int ind){
     if (ind > tables.size())
         return nullptr;
     return tables.at(ind);
-
 };
 
 const std::vector<BaseAction*> & Restaurant::getActionsLog() const
 {
-
+    return actionsLog;
 };
 
 std::vector<Dish>& Restaurant::getMenu()
@@ -206,7 +238,6 @@ DishType Restaurant::parseDishType (const std::string& dish)
 
 void Restaurant::insertNewDish(const std::string& currLine)
 {
-    int dishId = menu.size();
     std::string dishName;
     int dishPrice = -1;
     DishType dishType;
@@ -218,24 +249,22 @@ void Restaurant::insertNewDish(const std::string& currLine)
     {
         if (currLine.at(i) != ',')
             continue;
-        if (currDetail == 1)
+        if (currDetail == 1)    //dish name found
         {
             dishName = currLine.substr(0,i);
             lastIndex = i;
             currDetail++;
         }
-        else if (currDetail == 2)
+        else if (currDetail == 2)   //dish type and price found
         {
             dishType = parseDishType(currLine.substr(lastIndex+1,3));
-
             std::string::size_type sz;
             dishPrice = std::stoi (currLine.substr(i+1),&sz);
-
         }
 
     }
     if (dishPrice != -1)
-        menu.push_back(Dish(dishId,dishName,dishPrice,dishType));
+        menu.push_back(Dish(menu.size(),dishName,dishPrice,dishType));
 };
 
 
